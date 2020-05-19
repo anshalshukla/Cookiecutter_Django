35f56533d6cb1ce868d3dc6777fbcf0280a5de2b
Anshal Shukla <shukla.anshal85@gmail.com>
2020-05-19 09:23:45 +0530

    upstream changed.

--- {{cookiecutter.repo_name}}/entrypoint.sh
+++ {{cookiecutter.repo_name}}/entrypoint.sh
@@ -14,5 +14,6 @@ fi
 echo "Making migrations and migrating the database. "
 python manage.py makemigrations main --noinput 
 python manage.py migrate --noinput 
+python manage.py collectstatic --noinput
 
 exec "$@"

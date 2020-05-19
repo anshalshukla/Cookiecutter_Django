9388a494fe551b53247c711c9078c81a59c81b11
Anshal Shukla <shukla.anshal85@gmail.com>
2020-05-19 10:21:06 +0530

    prod server configured.

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = Secrets.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
